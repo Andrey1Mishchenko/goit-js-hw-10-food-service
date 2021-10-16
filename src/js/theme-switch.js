17d8b1594fa0c493814415d0d46f93a5c4f26231
Andrey1Mishchenko <andrej1mishchenko@gmail.com>
2021-10-16 23:48:31 +0300

    изменение цвета темы (без записи в локал сторейдж)

--- src/js/theme-switch.js
+++ src/js/theme-switch.js
@@ -6,6 +6,7 @@ const Theme = {
   DARK: 'dark-theme',
 };
 
+// рендеринг разметки элементов меню
 const menuEl = document.querySelector('.js-menu');
 const itemsMarkup = createMenuItemsMarkup(menuItems);
 
@@ -14,3 +15,13 @@ menuEl.insertAdjacentHTML('beforeend', itemsMarkup);
 function createMenuItemsMarkup(items) {
   return menuItemTpl(items);
 }
+// рендеринг разметки єэлементов меню
+
+// переключение темы. localStorage
+const bodyEl = document.querySelector('body');
+const themeSwitchToggle = document.querySelector('#theme-switch-toggle');
+themeSwitchToggle.addEventListener('change', changeThemeSwitchHandler);
+function changeThemeSwitchHandler(evt) {
+  bodyEl.classList.toggle(Theme.DARK);
+}
+// переключение темы. localStorage
