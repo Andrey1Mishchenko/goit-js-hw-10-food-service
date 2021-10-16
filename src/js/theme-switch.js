3aa2c9198fed57513e4480fd13d026a5d7ec0e64
Andrey1Mishchenko <andrej1mishchenko@gmail.com>
2021-10-16 23:33:48 +0300

    шаблонизация элементов меню и добавление в DOM

--- src/js/theme-switch.js
+++ src/js/theme-switch.js
@@ -1,5 +1,16 @@
+import menuItemTpl from '../templates/menu-card.hbs';
+import menuItems from '../menu.json';
+
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
-console.log(Theme.LIGHT);
+
+const menuEl = document.querySelector('.js-menu');
+const itemsMarkup = createMenuItemsMarkup(menuItems);
+
+menuEl.insertAdjacentHTML('beforeend', itemsMarkup);
+
+function createMenuItemsMarkup(items) {
+  return menuItemTpl(items);
+}
